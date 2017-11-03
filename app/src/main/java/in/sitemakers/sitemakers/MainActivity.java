edb6c459efd63e58c9e9f4eba09511721ea9ebb5
expertphpdevelopers <expertphpdevelopers@gmail.com>
2017-11-03 07:35:41 +0530

    design updated

--- app/src/main/java/in/sitemakers/sitemakers/MainActivity.java
+++ app/src/main/java/in/sitemakers/sitemakers/MainActivity.java
@@ -34,6 +34,7 @@ public class MainActivity extends AppCompatActivity
                         .setAction("Action", null).show();
             }
         });
+        
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
